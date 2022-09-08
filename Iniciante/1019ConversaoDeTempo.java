58a49cb411ef80c1396463f6c5d652c0dd91fdc8
Eric Eiti Nomura <eric.nomura33@gmail.com>
2022-09-08 16:19:10 -0300

    Segunda remessa de exercicios

---
+++ Iniciante/1019ConversaoDeTempo.java
@@ -0,0 +1,20 @@
+import java.io.IOException;
+import java.util.Scanner;
+
+public class Main {
+
+	public static void main(String[] args) throws IOException {
+		
+		Scanner sc = new Scanner(System.in);
+
+		int N = sc.nextInt();
+		
+		int horas = N / 3600;
+		int minutos = (N % 3600) / 60;
+		int segundos = (N % 3600) % 60;
+		
+		System.out.println(horas + ":" + minutos + ":" + segundos);
+		
+	}
+
+}
